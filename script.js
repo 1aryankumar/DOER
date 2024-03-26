5646a083f72758f3621df08eb045ada34e9d1aad
Aryan Kumar <yankumar101@gmail.com>
2024-03-26 17:55:16 +0530

    v4

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const inputBox =  document.getElementById('input-box');
+const inputBox =  document.getElementById("input-box");
 
-const listContainer =  document.getElementById('list-container');
-function addTasks(){
+const listContainer =  document.getElementById("list-container");
+
+function addTask(){
     if(inputBox.value === ''){
         alert("You must add a task")
 
     }
     else{
-        let li = document.createElement('li');
+        let li = document.createElement("li");
         li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
     }
